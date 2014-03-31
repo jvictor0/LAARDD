2e99243543aa39999f1378fe8aee52657c7bfe83
Joseph Victor <jvictor@stanford.edu>
2014-03-31 14:49:58 -0700

    Natural Spline Expansions (in 1-dimension) now working, and testing says they are behaving as theoretically expected. Yaaay nonlinear predictors. Smoothing Splines coming next.

--- Predictor.h
+++ Predictor.h
@@ -30,7 +30,7 @@ public:
   {
     arma::mat QtY;
     LAARDD::QRPair QR = LAARDD::SequentialQR(X, Y, &QtY);
-    assert(QR.R);
+    assert(QR.R);					
     arma::solve(m_beta_hat, arma::trimatu(*QR.R), QtY);
     std::cout << "beta_hat = " << m_beta_hat << std::endl;
     QR.Free();
@@ -67,7 +67,9 @@ public:
     QR.Free();
   }
 
-  static void Path(ShardedMatrix * X, ShardedMatrix * Y, arma::mat lambdas,std::vector<RidgeRegression> & result)
+  static void Path(ShardedMatrix * X, ShardedMatrix * Y, 
+		   double lambda_min, double lambda_max, uint num_lambdas, 
+		   std::vector<RidgeRegression> & result)
   {
     arma::mat QtY;
     LAARDD::QRPair QR = LAARDD::SequentialQR(X, Y, &QtY);
@@ -75,11 +77,11 @@ public:
     arma::mat U,V;
     arma::vec s;
     arma::svd(U,s,V,*QR.R);
-    result.reserve(lambdas.size());
+    result.reserve(num_lambdas);
     QtY = U.t() * QtY;
-    for (uint i = 0; i < lambdas.size(); ++i)
+    for (uint i = 0; i < num_lambdas; ++i)
     {
-      result.push_back(RidgeRegression(lambdas(i)));
+      result.push_back(RidgeRegression(lambda_min + i * (lambda_max - lambda_min) / (num_lambdas - 1)));
       result.back().Train_(V,s,QtY);
     }
     QR.Free();
@@ -113,7 +115,8 @@ private:
 //
 class BasisExpansionShardedMatrix : public ShardedMatrix
 {
-  BasisExpansionShardedMatrix() : ShardedMatrix(0) { }
+public:
+  BasisExpansionShardedMatrix(ShardedMatrix * mat) : ShardedMatrix(0,mat->SegmentsNumRows()), m_data_matrix(mat) { }
 
   bool WriteMatrixSegment(uint seg_num, arma::mat & out)
   {
@@ -144,7 +147,7 @@ class BasisExpansionShardedMatrix : public ShardedMatrix
   {
     return [col_num] (const arma::mat & data_segment, double * out)
     {
-      std::memcpy(out, data_segment.colptr(col_num), sizeof(uint) * data_segment.n_rows);
+      std::memcpy(out, data_segment.colptr(col_num), sizeof(double) * data_segment.n_rows);
     };
   }
 
@@ -154,14 +157,31 @@ class BasisExpansionShardedMatrix : public ShardedMatrix
     {
       for (uint i = 0; i < data_segment.n_rows; ++i)
       {
-	out[i] = std::pow(data_segment[col_data[0]], data_segment[col_data[1]]);
+	out[i] = std::pow(data_segment(i,col_data[0]), col_data[1]);
       }
       for (uint j = 2; j < col_data.size(); j += 2)
       {
 	for (uint i = 0; i < data_segment.n_rows; ++i)
 	{
-	  out[i] *= std::pow(data_segment[col_data[j]], data_segment[col_data[j+1]]);
+	  out[i] *= std::pow(data_segment(i,col_data[j]), col_data[j+1]);
 	}
+      }
+    };
+  }
+  
+  static ColumnFun NaturalCubicSplineColumn(uint col_num, double xi_k, double xi_K_1, double xi_K)
+  {
+    return [col_num, xi_k, xi_K_1, xi_K] (const arma::mat & data_segment, double * out)
+    {
+      for (uint i = 0; i < data_segment.n_rows; ++i)
+      {
+	double d_k = data_segment(i,col_num) - xi_k;
+	double d_K = data_segment(i,col_num) - xi_K;
+	double d_K_1 = data_segment(i,col_num) - xi_K_1;
+	double l_K = (data_segment(i,col_num) < xi_K) * d_K * d_K * d_K;
+	double l_K_1 = (data_segment(i,col_num) < xi_K_1) * d_K_1 * d_K_1 * d_K_1;
+	double l_k = (data_segment(i,col_num) < xi_k) * d_k * d_k * d_k;
+	out[i] = (l_k-l_K)/(xi_K - xi_k) - (l_K_1-l_K)/(xi_K - xi_K_1);
       }
     };
   }
@@ -201,6 +221,19 @@ class BasisExpansionShardedMatrix : public ShardedMatrix
 	base[2] = j;
 	AddColumn(MonomialColumn(base));
       }
+    }
+  }
+
+  // Adds a natural cubic spline without intercept, knots uniformly placed.
+  // TODO: knots should be placed according to empirical distribution, not uniformly, but I'm knot sure if it matters.  
+  //
+  void AddNaturalCubicSpline(uint col_num, double knot_0, double knot_final, uint num_knots)
+  {
+    AddColumn(CopyColumn(col_num));
+    double knot_final_1 = knot_0 + (num_knots - 2) * (knot_final - knot_0) / (num_knots - 1);
+    for (uint i = 0; i < num_knots - 2; ++i)
+    {
+      AddColumn(NaturalCubicSplineColumn(col_num, knot_0 + i * (knot_final - knot_0) / (num_knots - 1), knot_final_1, knot_final));
     }
   }
 
@@ -245,3 +278,4 @@ private:
   ShardedMatrix * m_data_matrix;
   std::vector<ColumnFun> m_basis_functions;
 };
+
--- PredictorTest.h
+++ PredictorTest.h
@@ -42,6 +42,48 @@ XYPair SimulateLM(uint s, uint p, uint b, double epsilon)
   return result;
 }
 
+#define PLUS_CUBED(X,knot) arma::pow(arma::max(arma::join_horiz(X-knot * arma::ones(X.n_rows,1), arma::zeros(X.n_rows,1)),1),3)
+
+XYPair SimulateNaturalCubicSpline(uint s, uint b, double epsilon, uint num_knots)
+{
+  DiskShardedMatrix *X = new DiskShardedMatrix(1);
+  DiskShardedMatrix *Y = new DiskShardedMatrix(1);
+  arma::mat XSeg,YSeg;
+  XYPair XY;
+  XY.XTrain = X;
+  XY.YTrain = Y;
+  arma::mat beta = arma::randn<arma::mat>(num_knots,1);
+  arma::vec knots = 2 * arma::randu<arma::mat>(num_knots);
+  for (uint i = 1; i < num_knots; ++i)
+  {
+    knots(i) = knots(i) + knots(i-1);
+  }
+  for (uint i = 0; i < b; ++i)
+  {
+    std::cout << "Simulating segment " << i << std::endl;
+    XSeg = num_knots * arma::ones(s,1) + arma::randn<arma::mat>(s,1) * num_knots;
+    YSeg = beta(0) * arma::ones(s,1) + beta(1) * XSeg;
+    for (uint j = 2; j < num_knots; ++j)
+    {
+      YSeg = YSeg + beta(j) *
+	((PLUS_CUBED(XSeg,knots(j-2)) - PLUS_CUBED(XSeg,knots(num_knots - 1)))/(knots(j-2) - knots(num_knots - 1)) 
+	 - (PLUS_CUBED(XSeg,knots(num_knots - 2)) - PLUS_CUBED(XSeg,knots(num_knots - 1)))/(knots(num_knots - 2) - knots(num_knots - 1)));
+    }
+    YSeg = YSeg + epsilon * arma::randn<arma::mat>(s,1);
+    if (i < b - 1)
+    {
+      X->AddSegment(XSeg);
+      Y->AddSegment(YSeg);
+    }
+    else
+    {
+      XY.XTest = XSeg;
+      XY.YTest = YSeg;
+    }
+  }
+  return XY;
+}
+
 bool TestLinearRegression(uint s, uint p, uint b, double epsilon)
 {
   XYPair XY = SimulateLM(s, p, b, epsilon);
@@ -58,19 +100,29 @@ bool TestLinearRegression(uint s, uint p, uint b, double epsilon)
 bool TestRidgeRegression(uint s, uint p, uint b, double epsilon, double lambda_min, double lambda_max, uint num_lambdas)
 {
   XYPair XY = SimulateLM(s, p, b, epsilon);
-  arma::vec lambdas(num_lambdas);
-  for (uint i = 0; i < num_lambdas; ++i)
-  {
-    lambdas(i) = lambda_min + i * (lambda_max - lambda_min) / (num_lambdas-1);
-  }
   std::vector<RidgeRegression> path;
-  RidgeRegression::Path(XY.XTrain ,XY.YTrain, lambdas, path);
+  RidgeRegression::Path(XY.XTrain ,XY.YTrain, lambda_min, lambda_max, num_lambdas, path);
   for (uint i = 0; i < num_lambdas; ++i)
   {
     arma::mat YHat = path[i].Predict(XY.XTest);
     double rms = arma::sum(arma::sum(arma::square(XY.YTest-YHat)))/s;
-    std::cout << "lambda = " << lambdas(i) << ", rms = " << rms << std::endl;
+    std::cout << "lambda = " << lambda_min + i * (lambda_max - lambda_min) / (num_lambdas - 1) << ", rms = " << rms << std::endl;
   }
   XY.Free();
   return true;
+}
+
+bool TestNaturalSplineRegression(uint s, uint b, double epsilon, uint num_knots_gen, uint num_knots_train)
+{
+  XYPair XY = SimulateNaturalCubicSpline(s, b, epsilon, num_knots_gen);
+  BasisExpansionShardedMatrix spline_model = BasisExpansionShardedMatrix(XY.XTrain);
+  spline_model.AddIntercept();
+  spline_model.AddNaturalCubicSpline(0, 0, 2.0* num_knots_gen, num_knots_train);
+  BasisExpansionShardedMatrix::Transformed<LinearRegression> * lm = BasisExpansionShardedMatrix::TransformModel<LinearRegression>(&spline_model, XY.YTrain);
+  arma::mat YHat = lm->Predict(XY.XTest);
+  double rms = arma::sum(arma::sum(arma::square(XY.YTest-YHat)))/s;
+  std::cout << "rms = " << rms << std::endl;
+  XY.Free();
+  //  delete lm;
+  return true;  
 }
